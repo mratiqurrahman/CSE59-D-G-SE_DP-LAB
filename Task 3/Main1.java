0d2d28534903290c24f52310c604e6b587ea70df
Atiqur Rahman <atiqurrahman.tech1@gmail.com>
2026-06-25 17:16:03 +0600

    Update Main1.java

--- Task 3/Main1.java
+++ Task 3/Main1.java
@@ -1,1 +1,45 @@
+public class Main{
+    public static void main(String[]args){
+        User user = new User("Atiq", "atiq@gmail.com");
+        if (EmailValidator.isValidEmail(user.getEmail())) {
+            UserRepository repository = new UserRepository();
+            repository.saveUser(user);
+            
+        }
+    }
+}
+class User{
+    private String username;
+    private String email;
 
+    public User(String username, String email){
+        this.username = username;
+        this.email = email;
+    }
+    public String getUsername(){
+        return username;
+
+    }
+    public String getEmail(){
+        return email;
+
+    }
+}
+
+class EmailValidator{
+    public static boolean
+    isValidEmail(String email){
+        if(email != null && email.contains("@")){
+            return true;
+        }
+        System.out.println("Invalid email format.");
+        return false;
+    }
+}
+
+class UserRepository{
+    public void saveUser(User user){
+        System.out.println("Connecting to database...");
+        System.out.println("Saving user " + user.getUsername() + " to the users table.");
+    }
+}
